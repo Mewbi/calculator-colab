f02d524752d9370bdbb1ec0177dfc2ce24f92b14
Felipe Fernandes <mewbi.fernandes@gmail.com>
2026-05-10 23:17:16 -0300

    Remove mensagem boilerplate

--- src/calculos_uteis/fatorial.c
+++ src/calculos_uteis/fatorial.c
@@ -7,8 +7,5 @@ void fatorial() {
     printf("  Essa funcionalidade ainda não está\n");
     printf("  implementada :(\n");
     printf("========================================\n");
-    printf("\n");
-    printf("  Que tal contribuir com o projeto?\n");
-    printf("  Acesse: github.com/SEU_USUARIO/calculadora-colaborativa\n");
-    printf("\n");
+
 }
